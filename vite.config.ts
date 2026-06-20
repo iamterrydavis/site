6f2f4f29bbc32c1d6f88cd389b015bcf26ba71ca
swat <241602758+iamterrydavis@users.noreply.github.com>
2026-06-20 23:36:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { tanstackRouter } from "@tanstack/router-vite-plugin";
-import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tanstackRouter()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
+  plugins: [react()],
 });
