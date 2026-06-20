61864533293e7d25389e25bab7bd57a926288d2d
swat <241602758+iamterrydavis@users.noreply.github.com>
2026-06-20 23:17:05 +0200

    Create __root.tsx

---
+++ src/routes/__root.tsx
@@ -0,0 +1,13 @@
+import {
+  Outlet,
+  createRootRouteWithContext,
+} from "@tanstack/react-router";
+import { QueryClient } from "@tanstack/react-query";
+
+export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return <Outlet />;
+}
